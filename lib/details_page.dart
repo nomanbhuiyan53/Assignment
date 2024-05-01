14765dd397b99ee7b3a5e10c5fd60c95f78da973
nomanbhuiyan53 <noman.bhuyan@gmail.com>
2024-05-01 11:45:49 +0600

    dd

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -40,148 +40,152 @@ class DetailsPage extends StatelessWidget {
             if (orientation == Orientation.landscape) {
               return Padding(
                 padding: const EdgeInsets.all(8.0),
-                child:Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.4,
-                        height: 300,
-                        decoration: BoxDecoration(
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.4),
-                                spreadRadius: 2,
-                                blurRadius: 4,
-                                offset: Offset(0, 3), // changes position of shadow
-                              ),
-                            ],
-                            borderRadius: BorderRadius.circular(10),
-                            image: DecorationImage(
-                              image: AssetImage(img),
-                              fit: BoxFit.cover,
-                            )),
-                      ),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.56,
-                      child: Padding(
+                child:SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Row(
+                    children: [
+                      Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width,
-                              child: Text(
-                                'Mood with nature',textAlign: TextAlign.start,
-                                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            const Wrap(children: [
-                              Text(
-                                'Being in nature, or even viewing scenes of nature, reduces anger, fear, and stress and increases pleasant feelings',
-                                textAlign: TextAlign.justify,
-                                style: TextStyle(
-                                  fontSize: 16,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          height: 300,
+                          decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.4),
+                                  spreadRadius: 2,
+                                  blurRadius: 4,
+                                  offset: Offset(0, 3), // changes position of shadow
                                 ),
-                              ),
-                            ]),
-                            SizedBox(height: 10),
-                            SizedBox(
-                              width: double.infinity,
-                              child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.green,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(50),
-                                  ),
-                                ),
-                                onPressed: () {},
-                                child: const Text(
-                                  'See More',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            Expanded(
-                              child: Container(
-                                height: MediaQuery.of(context).size.height,
-                                child: GridView.count(
-                                    primary: false,
-                                    padding: const EdgeInsets.all(10),
-                                    crossAxisSpacing: 10,
-                                    mainAxisSpacing: 10,
-                                    crossAxisCount: 2,
-                                    children: [
-                                      InkWell(
-                                        onTap: () {},
-                                        child: Container(
-                                          decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.circular(20),
-                                            image: const DecorationImage(
-                                              image:
-                                              AssetImage('assets/image/image-3.jpg'),
-                                              // Path to your image asset
-                                              fit: BoxFit
-                                                  .cover, // Adjust the image to cover the entire area
-                                            ),
-                                            // radius of 10
-                                          ),
-                                          child: const Align(
-                                            alignment: Alignment.bottomLeft,
-                                            child: Padding(
-                                              padding: EdgeInsets.all(10.0),
-                                              child: Text(
-                                                'Animals',
-                                                style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 18,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                      InkWell(
-                                        onTap: () {},
-                                        child: Container(
-                                          decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.circular(10),
-                                            image: const DecorationImage(
-                                              image:
-                                              AssetImage('assets/image/image-5.jpg'),
-                                              // Path to your image asset
-                                              fit: BoxFit
-                                                  .cover, // Adjust the image to cover the entire area
-                                            ),
-                                            // radius of 10
-                                          ),
-                                          child: const Align(
-                                            alignment: Alignment.bottomLeft,
-                                            child: Padding(
-                                              padding: EdgeInsets.all(10.0),
-                                              child: Text(
-                                                'Sky',
-                                                style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 18,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ]),
-                              ),
-                            ),
-                          ],
+                              ],
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                image: AssetImage(img),
+                                fit: BoxFit.cover,
+                              )),
                         ),
                       ),
-                    ),
-                  ],
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.56,
+                        height: MediaQuery.of(context).size.height,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: Text(
+                                  'Mood with nature',textAlign: TextAlign.start,
+                                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              const Wrap(children: [
+                                Text(
+                                  'Being in nature, or even viewing scenes of nature, reduces anger, fear, and stress and increases pleasant feelings',
+                                  textAlign: TextAlign.justify,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ]),
+                              SizedBox(height: 10),
+                              SizedBox(
+                                width: double.infinity,
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.green,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(50),
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                  child: const Text(
+                                    'See More',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Expanded(
+                                child: Container(
+                                  height: MediaQuery.of(context).size.height,
+                                  child: GridView.count(
+                                      primary: false,
+                                      padding: const EdgeInsets.all(10),
+                                      crossAxisSpacing: 10,
+                                      mainAxisSpacing: 10,
+                                      crossAxisCount: 2,
+                                      children: [
+                                        InkWell(
+                                          onTap: () {},
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.circular(20),
+                                              image: const DecorationImage(
+                                                image:
+                                                AssetImage('assets/image/image-3.jpg'),
+                                                // Path to your image asset
+                                                fit: BoxFit
+                                                    .cover, // Adjust the image to cover the entire area
+                                              ),
+                                              // radius of 10
+                                            ),
+                                            child: const Align(
+                                              alignment: Alignment.bottomLeft,
+                                              child: Padding(
+                                                padding: EdgeInsets.all(10.0),
+                                                child: Text(
+                                                  'Animals',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 18,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                        InkWell(
+                                          onTap: () {},
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.circular(10),
+                                              image: const DecorationImage(
+                                                image:
+                                                AssetImage('assets/image/image-5.jpg'),
+                                                // Path to your image asset
+                                                fit: BoxFit
+                                                    .cover, // Adjust the image to cover the entire area
+                                              ),
+                                              // radius of 10
+                                            ),
+                                            child: const Align(
+                                              alignment: Alignment.bottomLeft,
+                                              child: Padding(
+                                                padding: EdgeInsets.all(10.0),
+                                                child: Text(
+                                                  'Sky',
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 18,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ]),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 )
 
               );
